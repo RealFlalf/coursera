304f12a42e2f7aac0620d1875dc92db5028f3950
Tsed <realflalf@gmail.com>
2020-09-04 16:11:04 +0300

    hz che ne tak

--- solution.py
+++ solution.py
@@ -51,34 +51,37 @@ class SpecMachine(CarBase):
 
 def get_car_list(csv_filename):
     car_list = []
-    with open(file_path) as csv_fd:
-        reader = csv.DictReader(csv_fd, delimiter=';')
-        # next(reader)  # пропускаем заголовок
-        for row in reader:
-            try:
-                if row["car_type"] == "car":
-                    car_list.append(Car(row["brand"], row["photo_file_name"], row["carrying"], row["passenger_seats_count"]))
-                elif row["car_type"] == "truck":
-                    car_list.append(Truck(row["brand"], row["photo_file_name"], row["carrying"], row["body_whl"]))
-                elif row["car_type"] == "spec_machine":
-                    car_list.append(SpecMachine(row["brand"], row["photo_file_name"], row["carrying"], row["extra"]))
-                else:
+    try:
+        with open(file_path) as csv_fd:
+            reader = csv.DictReader(csv_fd, delimiter=';')
+            # next(reader)  # пропускаем заголовок
+            for row in reader:
+                try:
+                    if row["car_type"] == "car":
+                        car_list.append(Car(row["brand"], row["photo_file_name"], row["carrying"], row["passenger_seats_count"]))
+                    elif row["car_type"] == "truck":
+                        car_list.append(Truck(row["brand"], row["photo_file_name"], row["carrying"], row["body_whl"]))
+                    elif row["car_type"] == "spec_machine":
+                        car_list.append(SpecMachine(row["brand"], row["photo_file_name"], row["carrying"], row["extra"]))
+                    else:
+                        pass
+                except (ValueError, TypeError):
                     pass
-            except (ValueError, TypeError):
-                pass
+    except NameError:
+        pass
     return car_list
 
 
 if __name__ == '__main__':
     file_path = "coursera_week3_cars.csv"
-    cars = get_car_list(file_path)
-
-    car = Car('Bugatti Veyron', 'bugatti.png', '0.312', '2')
-    print(car.car_type, car.brand, car.photo_file_name, car.carrying, car.passenger_seats_count, sep='\n')
-    truck = Truck('Nissan', 't1.jpg', '2.5', '2.4x2.3x2')
-    print(truck.car_type, truck.brand, truck.photo_file_name, truck.body_length, truck.body_width, truck.body_height, sep='\n')
-    spec_machine = SpecMachine('Komatsu-D355', 'd355.jpg', '93', 'pipelayer specs')
-    print(spec_machine.car_type, spec_machine.brand, spec_machine.carrying, spec_machine.photo_file_name, spec_machine.extra, sep='\n')
-    spec_machine.get_photo_file_ext()
-    cars = get_car_list(file_path)
+    # cars = get_car_list(file_path)
+    #
+    # car = Car('Bugatti Veyron', 'bugatti.png', '0.312', '2')
+    # print(car.car_type, car.brand, car.photo_file_name, car.carrying, car.passenger_seats_count, sep='\n')
+    # truck = Truck('Nissan', 't1.jpg', '2.5', '2.4x2.3x2')
+    # print(truck.car_type, truck.brand, truck.photo_file_name, truck.body_length, truck.body_width, truck.body_height, sep='\n')
+    # spec_machine = SpecMachine('Komatsu-D355', 'd355.jpg', '93', 'pipelayer specs')
+    # print(spec_machine.car_type, spec_machine.brand, spec_machine.carrying, spec_machine.photo_file_name, spec_machine.extra, sep='\n')
+    # spec_machine.get_photo_file_ext()
+    cars = get_car_list('coursera_week3_cars.csv')
     print(len(cars))
